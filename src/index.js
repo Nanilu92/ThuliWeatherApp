f996f205c6a03f69406c3b0439ab680e0112675f
Nanilu92 <167389140+Nanilu92@users.noreply.github.com>
2024-07-16 14:54:29 +0200

    Added forecast infomation on index.js

--- src/index.js
+++ src/index.js
@@ -50,7 +50,41 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "50fct75763624137a3d6f06o080e5bf5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function displayForecast(response) {
+  let forecastHTML = "";
+  response.data.daily.forEach(function(day, index)) }
+    if (index < 5) {
+      forecastHTML =
+      forecast + 
+      <div class="weather-forecast-single-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${
+          day.condition.icon_url
+    }" class="weather-forecast-icon" />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max">${Math.round(day.tempersture.maximum)}°</span>
+      <span class="weather-forecast-temperature-max">${Math.round(day.tempersture.min)}°</span>
+      </div>
+      </div>
+      `;
+  }
+});
+let forecast = document.querySelector("#forecast);
+forecast.innerHTML = forecastHTML;
+    }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("Mthatha");
